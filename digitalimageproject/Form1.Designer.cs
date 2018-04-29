3a20370cec608cc7cc9e6f0c6c9f406b225f0fae
asad khan <auzair535@gmail.com>
2018-04-29 19:53:00 +0500

    dip project

---
+++ digitalimageproject/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace digitalimageproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(704, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(704, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Smokey";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(704, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Banana Bomb";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(704, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 30);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Grey Scale";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(704, 156);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 30);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Electric";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.Location = new System.Drawing.Point(704, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 30);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Wall";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Control;
+            this.button7.Location = new System.Drawing.Point(704, 228);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(131, 30);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Winter";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Red";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(53, 408);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(650, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(53, 442);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(650, 45);
+            this.trackBar2.TabIndex = 11;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "green";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(53, 474);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(650, 45);
+            this.trackBar3.TabIndex = 13;
+            this.trackBar3.ValueChanged += new System.EventHandler(this.trackBar3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "blue";
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Control;
+            this.button8.Location = new System.Drawing.Point(704, 404);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(126, 44);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Save Image";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.Location = new System.Drawing.Point(704, 454);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(126, 44);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "Open Image";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(703, 389);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(704, 387);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(835, 510);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pic Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ digitalimageproject/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace digitalimageproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // our group use this function to open image in picture box 
+        void openImage() {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK) {
+
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+        
+        }
+        
+        //this function is to save the image 
+        void saveImage() {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.png;|*.jpg;|*.bmp;"; //our app read only these formats of images
+            ImageFormat form =  ImageFormat.Png; // our resulted pic will save in png format
+            if(sfd.ShowDialog()==System.Windows.Forms.DialogResult.OK){
+                
+                string ext =Path.GetExtension(sfd.FileName);
+                switch(ext)
+                {
+                    case ".jpg":
+                        form=ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        form=ImageFormat.Bmp;
+                        break;
+                
+                }
+                pictureBox1.Image.Save(sfd.FileName,form);
+            }
+
+  }
+
+        void reloadImage() {
+
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            else { 
+            
+            if(opened){
+
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+            }
+        
+        }
+
+        void filter1() {
+            if (!opened) {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width,img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix( new float[][] { new float[] {.393f,.349f+0.5f,.272f,0,0},//red
+             new float[] {.769f+0.3f,.686f,.534f,0,0},// for green
+             new float[] {.189f,.168f,.131f+0.5f,0,0},// blue
+             new float[] {0,0,0,1,0},
+             new float[] {0,0,0,0,1}  });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img , new Rectangle(0 , 0 , img.Width , img.Height),0 , 0 ,img.Width,img.Height,GraphicsUnit.Pixel,ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+        
+        }
+
+
+        void filter2()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] { new float[] {.53f,.39f,0,0,0},//red
+             new float[] {.769f+0.3f,.986f,.534f,0,0},// for green
+             new float[] {.189f,2.168f,0,0,0},// blue
+             new float[] {0,0,0,1,0},
+             new float[] {0,0,0,0,1}  });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+        void filter3()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] {  new float[]{0.299f, 0.299f, 0.299f, 0, 0},
+                    new float[]{0.587f, 0.587f, 0.587f, 0, 0},
+                    new float[]{0.114f, 0.114f, 0.114f, 0, 0},
+                    new float[]{0, 0, 0, 1, 0},
+                    new float[]{0, 0, 0, 0, 0} });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+
+        void filter4()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] { new float[]{1+0.9f, 0, 0, 0, 0},
+            new float[]{0, 1+1.5f, 0, 0, 0},
+            new float[]{0, 0, 1+1.3f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1} });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+
+        void filter5()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] {new float[]{.393f, .349f, .272f, 0, 0},
+            new float[]{.769f, .686f, .534f, 0, 0},
+            new float[]{.189f, .168f, .131f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1} });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+
+        void filter6()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] {new float[]{1+0.3f, 0, 0, 0, 0},
+            new float[]{0, 1+0f, 0, 0, 0},
+            new float[]{0, 0, 1+5f, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1} });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+
+        void hue()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("open an image first");
+            }
+
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            ImageAttributes ia = new ImageAttributes();
+            ColorMatrix cm = new ColorMatrix(new float[][] { new float[] {1+(trackBar1.Value*0.1f),0,0,0,0},//red
+             new float[] {0,1+(trackBar2.Value*0.1f),0,0,0},// for green
+             new float[] {0,0,1+(trackBar3.Value*0.1f),0,0},// blue
+             new float[] {0,0,0,1,0},
+             new float[] {0,0,0,0,1}  });
+            ia.SetColorMatrix(cm);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            g.Dispose();
+            pictureBox1.Image = bmp;
+
+        }
+
+         
+        Image file;
+        bool opened = false;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter3();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            openImage(); // our group created the function above and use that function on open image button event to open image 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // this button is to save image 
+            saveImage();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = 0;
+            trackBar2.Value = 0;
+            trackBar3.Value = 0;
+            reloadImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter2();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void trackBar3_ValueChanged(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter4();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter5();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            reloadImage();
+            filter6();
+        }
+    }
+}
